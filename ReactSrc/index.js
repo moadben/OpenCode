7b99f472e0c43d3242a0262c726887625498c0c9
Jason <jason741852@gmail.com>
2017-03-18 14:56:13 -0700

    Change title

---
+++ ReactSrc/index.js
@@ -0,0 +1,9 @@
+const { render } = ReactDOM
+
+render(
+	<h1 id='title'
+		className='header'
+		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
+Open Code	</h1>,
+	document.getElementById('container')
+)
