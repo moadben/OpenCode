3e4543248fc9005c0d074f8e67b16cc1eafbfe1e
QuinnHou <queiyun.hou@gmail.com>
2017-03-19 04:38:53 -0700

    Styled ProjForm and ideaForm with CSS

--- ReactSrc/ProjForm.js
+++ ReactSrc/ProjForm.js
@@ -1,3 +1,5 @@
+//import 'css/ProjForm.scss'
+
 var ProjForm = React.createClass({
   propTypes: {
     value: React.PropTypes.object.isRequired,
@@ -44,57 +46,62 @@ var ProjForm = React.createClass({
     var errors = this.props.value.errors || {};
 
     return(
+
+
       React.createElement('form', {onSubmit: this.onSubmit, className: 'ProjForm', noValidate: true},
         React.createElement('input', {
           type: 'text',
-          className: errors.title && 'ProjForm-error',
-          placeholder: 'Title (required)',
+          className: 'title-field',//errors.title && 'ProjForm-error',
+          placeholder: 'Title *',
           value: this.props.value.title,
           onChange: this.onTitleChange,
         }),
         React.createElement('input', {
           type: 'text',
-          className: errors.giturl && 'ProjForm-error',
-          placeholder: 'Git URL (required)',
+          className: 'git-field',//errors.giturl && 'ProjForm-error',
+          placeholder: 'Git URL *',
           value: this.props.value.giturl,
           onChange: this.onGiturlChange,
         }),
         React.createElement('textarea', {
           type: 'text',
-          className: errors.description && 'ProjForm-error',
-          placeholder: 'Description (required)',
+          className: 'description', //errors.description && 'ProjForm-error',
+          placeholder: 'Description *',
           value: this.props.value.description,
           onChange: this.onDescriptionChange,
         }),
         React.createElement('input', {
           type: 'text',
-          className: errors.dependency && 'ProjForm-error',
-          placeholder: 'Dependencies (required)',
+          className: 'dependency', //errors.dependency && 'ProjForm-error',
+          placeholder: 'Dependencies *',
           value: this.props.value.dependency,
           onChange: this.onDependencyChange,
         }),
         React.createElement('input', {
           type: 'text',
-          className: errors.appport && 'ProjForm-error',
-          placeholder: 'App Port (required)',
+          className: 'apport', //errors.appport && 'ProjForm-error',
+          placeholder: 'App Port *',
           value: this.props.value.appport,
           onChange: this.onAppportChange,
         }),
         React.createElement('textarea', {
           type: 'text',
-          className: errors.initcommand && 'ProjForm-error',
-          placeholder: 'Init Command (required)',
+          className: 'initcommand', //errors.initcommand && 'ProjForm-error',
+          placeholder: 'Init Command *',
           value: this.props.value.initcommand,
           onChange: this.onInitcommandChange,
         }),
         React.createElement('textarea', {
           type: 'text',
-          className: errors.runcommand && 'ProjForm-error',
-          placeholder: 'Run Command (required)',
+          className: 'runcommand', //errors.runcommand && 'ProjForm-error',
+          placeholder: 'Run Command *',
           value: this.props.value.runcommand,
           onChange: this.onRuncommandChange,
         }),
-        React.createElement('button', {type: 'submit'}, "Add Project")
+        React.createElement('button', {
+          type: 'submit',
+          className: 'submit',
+        }, "ADD PROJECT")
       )
     );
   },
@@ -109,7 +116,8 @@ var ProjItem = React.createClass({
 
   render: function() {
     return (
-      React.createElement('li', {className: 'ProjItem'},
+        React.createElement('li', {className: 'ProjItem'},
+        React.createElement('h1', {className: 'Proj-title'}, "CREATE AN IDEA"),
         React.createElement('h2', {className: 'ProjItem-title'}, this.props.title),
         React.createElement('h2', {className: 'ProjItem-giturlname'}, this.props.giturl),
         React.createElement('div', {className: 'ProjItem-description'}, this.props.description)
@@ -133,14 +141,17 @@ var ProjView = React.createClass({
       .map(function(proj) { return React.createElement(ProjItem, proj); });
 
     return (
+      React.createElement('div', {className: 'ProjView'},
+        React.createElement('h1', {className: 'ProjView-title'}, "CREATE AN PROJECT"),
         React.createElement(ProjForm, {
           value: this.props.newProj,
           onChange: this.props.onNewProjChange,
           onSubmit: this.props.onNewProjSubmit,
         })
-      );
-    }
-  });
+      )
+    );
+  },
+});
 
 
 
@@ -175,7 +186,7 @@ function setState(changes){
       onNewProjChange: updateNewProj,
       onNewProjSubmit: submitNewProj,
     })),
-    document.getElementById('container')
+    document.getElementById('projectContainer')
   );
 }
 
--- ReactSrc/home.js
+++
@@ -1,3 +0,0 @@
-toHome(){
-  return 
-}
