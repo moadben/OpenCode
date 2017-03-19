649ab6b02a9192015306d0e3ec2638bddd11d2a3
megelo <dakesiau@hotmail.com>
2017-03-18 22:16:34 -0700

    Implement reading methods for projects and ideas

--- ReactSrc/index.js
+++
@@ -1,9 +0,0 @@
-const { render } = ReactDOM
-
-render(
-	<h1 id='title'
-		className='header'
-		style={{backgroundColor: 'orange', color: 'white', fontFamily: 'verdana'}}>
-Hello</h1>,
-	document.getElementById('container')
-)
