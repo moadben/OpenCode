00dda5ddec4b79509ee38060da946d0bb691ea6b
soxies1 <tyson_bulmer@hotmail.com>
2017-03-18 23:48:08 -0700

    Add temporary file serving

--- NodeServer/app.js
+++ NodeServer/app.js
@@ -1,9 +1,14 @@
 var express = require('express')
 var app = express()
+var path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+//app.get('/', function (req, res) {
+//  res.send('Hello World!')
+//})
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
