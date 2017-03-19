99e28910b4943b44e33589cf4923b3af0bbb24f4
Jason <jason741852@gmail.com>
2017-03-19 09:39:56 -0700

    Implement project list into react

--- NodeServer/static/parseproject.js
+++ NodeServer/static/parseproject.js
@@ -5,6 +5,7 @@ var Pproject = React.createClass({
     };
   },
 
+
   componentDidMount: async function() {
     const request = await fetch('http://opencode.me:8080/projects');
     const projects = await request.json();
@@ -26,6 +27,45 @@ var Pproject = React.createClass({
               <p>{project.title}</p>
               <p>{project.description}</p>
               <ul>
+              <div id="start" class="modalDialog">
+                <div> <a href="#close" title="Close" class="close">X</a>
+
+                  <div class="innerModal" id="start">
+                    <h1>Steps to environment set up</h1>
+                    <br/>
+                    <!-- Place this tag where you want the button to render. -->
+                    <h3>1. Fork the Repo of the selected project</h3>
+                    <a class="github-button" href="{project.giturl}/fork" data-icon="octicon-repo-forked" data-style="mega" data-count-href="{project.userid}/{project.title}/network" data-count-api="/repos/{project.userid}/{project.title}#forks_count" data-count-aria-label="# forks on GitHub" aria-label="Fork {project.userid}/{project.title} on GitHub">Fork</a>
+                    <br/>
+
+                    <ul style="list-style:none;">
+                    <script src="js/fileGeneration.js"></script>
+                      <li>
+                        X. Open a terminal
+                      </li>
+                      <li>
+                        X. Create a project folder
+                        <span>mkdir OpenCodeProjects; cd OpenCodeProjects</span>
+                      </li>
+                      <li>
+                        X. Clone the newly forked repo
+                       <span>git clone {project.giturl}; cd {project.title}</span>
+                      </li>
+                    </ul>
+                    <br/>
+                    <h3>3. Download generated Dockerfile</h3>
+                    <button class="docButton" onclick="generateDockerFile(TestNodeApp);">Download Dockerfile</button>
+                    <br/>
+                    <br/>
+                    <h3>4. Download Generated Run Scripts</h3>
+                    <button class="docButton" onclick="generateRunScriptFile(TestNodeApp);">Download Run Scripts</button>
+                    <br/>
+                    <br/>
+                    <h3>5. Or Download Generated Super Script</h3>
+                    <button class="docButton" onclick="generateSuperScriptFile(TestNodeApp);">Download Super Script</button>
+                  </div>
+                </div>
+              </div>
                 {
                   project.discussion.comments.map(comment => <li key={comment.comment}>{comment.comment}</li>)
                 }
@@ -40,4 +80,4 @@ var Pproject = React.createClass({
   }
 });
 
-ReactDOM.render(<Pproject/>, document.getElementById('ooo'));
+ReactDOM.render(<Pproject/>, document.getElementById('projectSec'));
